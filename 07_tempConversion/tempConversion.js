398bfb978eac0a3fa66d7f5f4ad404a81ea9e85a
kenneth-r-edwards <edwardskenneth413@gmail.com>
2022-05-12 07:29:58 -0500

    finished ex 7

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,9 +1,11 @@
-const ftoc = function() {
-
+const ftoc = function (f) {
+  let num = (f - 32) * 5 / 9;
+  return Math.round(num * 10) / 10;
 };
 
-const ctof = function() {
-
+const ctof = function (c) {
+  let num = c * 9 / 5 + 32;
+  return Math.round(num * 10) / 10;
 };
 
 // Do not edit below this line
