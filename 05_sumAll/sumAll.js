5c40e7b36108a32b2ccfadf2d55bc623299609a3
kenneth-r-edwards <edwardskenneth413@gmail.com>
2022-05-12 07:10:54 -0500

    finished ex 5

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,23 @@
-const sumAll = function() {
+const sumAll = function (one, two) {
+    let start = (one < two) ? one : two;
+    let end = (one < two) ? two : one;
 
-};
+
+    let sum = 0;
+    for (let i = start; i <= end; i++) {
+        sum += i;
+    }
+
+    if ((one < 0) || (two < 0)) {
+        return 'ERROR';
+    }
+
+    if ((typeof (one) !== 'number') || (typeof (two) !== 'number')) {
+        return 'ERROR';
+
+    };
+    return sum;
+}
 
 // Do not edit below this line
 module.exports = sumAll;
