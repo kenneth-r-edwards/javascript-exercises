07635cb4b784200e774fcc0fd96b42135b4bb3ae
kenneth-r-edwards <edwardskenneth413@gmail.com>
2022-05-11 21:54:01 -0500

    completed ex 3

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,8 @@
-const reverseString = function() {
-
+const reverseString = function (string) {
+    strAry = Array.from(string);
+    revStrAry = strAry.reverse();
+    revStr = revStrAry.join("");
+    return revStr;
 };
 
 // Do not edit below this line
