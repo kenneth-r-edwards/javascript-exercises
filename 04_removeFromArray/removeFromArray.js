ddc079d98509bbcaeed67b2eaf49b76ea2996094
kenneth-r-edwards <edwardskenneth413@gmail.com>
2022-05-12 06:41:25 -0500

    completed ex 4

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,14 @@
-const removeFromArray = function() {
+const removeFromArray = function (Ary, ...nums) {
+    let args = [...nums];
 
+    for (const arg of args) {
+        for (let i = 0; i < Ary.length; i++) {
+            if (arg === Ary[i]) {
+                Ary.splice(i, 1);
+            }
+        }
+    }
+    return Ary;
 };
 
 // Do not edit below this line
